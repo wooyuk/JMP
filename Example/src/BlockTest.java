39d2a1916b24455d741897f6698d489d1c57d046
wooyuk <clannadm001@gmail.com>
2010-08-01 21:28:09 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example/src/BlockTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class BlockTest {
+    public static void main(String args[])  {
+        int x,y;
+        y=20;
+        for(x=0;x<10;x++)
+        {
+            System.out.println("This is x:"+x);
+            System.out.println("This is y"+y);
+            y=y-2;
+        }
+    }
+
+}
