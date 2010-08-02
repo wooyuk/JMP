125ca405f7e6637de4ea2fd5c3bb364399fea935
wooyuk <clannadm001@gmail.com>
2010-08-02 11:36:10 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example/src/DynInit.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+public class DynInit {
+    public static void main(String args[])  {
+        double a=3.0,b=4.0;
+        double c=Math.sqrt(a*a+b*b);
+        System.out.println("Hypotenuse is "+c);
+    }
+
+}
