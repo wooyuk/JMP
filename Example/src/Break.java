9749bae02499d6848bb8ec7257a75c496f439504
wooyuk <clannadm001@gmail.com>
2010-08-04 00:49:42 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example/src/Break.java
@@ -0,0 +1,21 @@
+/**
+ *  Just for fun
+ * 
+ */
+public class Break {
+    public static void main(String args[])  {
+        boolean t = true;
+
+        first:  {
+            second:  {
+                third:  {
+                    System.out.println("Before the break.");
+                    if(t)  break second;  //break out of second block
+                    System.out.println("This won't execute");
+                }
+                System.out.println("This won't execute");
+            }
+            System.out.println("This is after second block.");
+        }
+    }
+}
---
+++ Example/src/Continue.java
@@ -0,0 +1,13 @@
+/**
+ *  Just for fun
+ * 
+ */
+public class Continue {
+    public static void main(String args[])  {
+        for(int i=0; i<10; i++)  {
+            System.out.print(i + " ");
+            if(i%2 == 0)  continue;
+            System.out.println("");
+        }
+    }
+}
