c6e606cd487860f2dc21f3a3c47b71ed9ee6a7d9
wooyuk <clannadm001@gmail.com>
2010-08-06 19:35:41 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example/src/Balance2.java
@@ -0,0 +1,29 @@
+/**
+ *  Just for fun
+ * 
+ */
+package MyPack;
+
+/* Now, the Balance class, its constructor, and its
+   show() method are public.  This means that they can
+   be used by non-subclass code outside their package.
+*/
+public class Balance2 {
+  String name;
+  double bal;
+
+  public Balance2(String n, double b) {
+    name = n;
+    bal = b;
+  }
+
+  public void show() {
+    if(bal<0)
+      System.out.print("-->> ");
+    System.out.println(name + ": $" + bal);
+  }
+  public static void main(String args[]) {
+      System.out.println("Hi");
+  }
+}
+
