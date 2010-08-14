593a62d9404e50349568030b16b7efdf9b02ee5a
wooyuk <clannadm001@gmail.com>
2010-08-14 12:06:50 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example/src/DirListOnly.java
@@ -0,0 +1,19 @@
+/**
+ *  Just for fun
+ * 
+ */
+// Directory of .HTML files.
+import java.io.*;
+
+class DirListOnly {
+  public static void main(String args[]) {
+    String dirname = "/java";
+    File f1 = new File(dirname);
+    FilenameFilter only = new OnlyExt("html");
+    String s[] = f1.list(only);
+
+    for (int i=0; i < s.length; i++) {
+      System.out.println(s[i]);
+    }
+  }
+}
---
+++ Example/src/OnlyExt.java
@@ -0,0 +1,17 @@
+/**
+ *  Just for fun
+ * 
+ */
+import java.io.*;
+
+public class OnlyExt implements FilenameFilter {
+  String ext;
+
+  public OnlyExt(String ext) {
+    this.ext = "." + ext;
+  }
+
+  public boolean accept(File dir, String name) {
+    return name.endsWith(ext);
+  }
+}
