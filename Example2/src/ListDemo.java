066dcb3ddcf4bd2f4578da696019dba333ae9492
wooyuk <clannadm001@gmail.com>
2010-08-18 11:48:37 +0800

    Signed-off-by: wooyuk <clannadm001@gmail.com>

---
+++ Example2/src/ListDemo.java
@@ -0,0 +1,62 @@
+/**
+ *  Just for fun
+ *
+ */
+
+// Demonstrate Lists.
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+/*
+  <applet code="ListDemo" width=300 height=180>
+  </applet>
+*/
+
+public class ListDemo extends Applet implements ActionListener {
+  List os, browser;
+  String msg = "";
+
+  public void init() {
+    os = new List(4, true);
+    browser = new List(4, false);
+
+    // add items to os list
+    os.add("Windows 98/XP");
+    os.add("Windows NT/2000");
+    os.add("Solaris");
+    os.add("MacOS");
+
+    // add items to browser list
+    browser.add("Internet Explorer");
+    browser.add("Firefox");
+    browser.add("Opera");
+
+    browser.select(1);
+
+    // add lists to window
+    add(os);
+    add(browser);
+
+    // register to receive action events
+    os.addActionListener(this);
+    browser.addActionListener(this);
+  }
+
+  public void actionPerformed(ActionEvent ae) {
+    repaint();
+  }
+
+  // Display current selections.
+  public void paint(Graphics g) {
+    int idx[];
+
+    msg = "Current OS: ";
+    idx = os.getSelectedIndexes();
+    for(int i=0; i<idx.length; i++)
+      msg += os.getItem(idx[i]) + "  ";
+    g.drawString(msg, 6, 120);
+    msg = "Current Browser: ";
+    msg += browser.getSelectedItem();
+    g.drawString(msg, 6, 140);
+  }
+}
